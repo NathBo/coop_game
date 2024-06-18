90f0427d54c6607bb74edb80d2053af93a89a6f4
Vincent <j.vinc-sites@yahoo.com>
2024-06-18 23:15:06 +0200

    definie bien l'id de cq object

--- read_map.js
+++ read_map.js
@@ -8,20 +8,27 @@ function initialize3DArray(x, y, z, val) {
 
 
 
-function read_map(obj_init){
-    console.log(obj_init);
-    let largeur = obj_init.obstacles[0][0].length;
-    let hauteur = obj_init.obstacles[0].length;
-
-    var object_final = obj_init;
+function read_map(map){
+    let largeur = map.obstacles[0][0].length;
+    let hauteur = map.obstacles[0].length;
+    let nb_objects = map.objects.length;
+    
+    // rotate map
     var new_obstacles = initialize3DArray(2, largeur, hauteur, '#' );
     for (let player = 0; player < 2; player++)
         for (let x = 0; x < largeur; x++)
             for (let y = 0; y < hauteur; y++)
-                new_obstacles[player][x][y] = obj_init.obstacles[player][y][x];
-    
-    object_final.obstacles = new_obstacles;
-    return object_final;
+                new_obstacles[player][x][y] = map.obstacles[player][y][x];
+    map.obstacles = new_obstacles;
+
+
+    // well define the id for each objects
+    for (let i_object = 0; i_object < nb_objects; i_object++)  {
+        if (map.objects[i_object].id === undefined)
+            map.objects[i_object].id = 'new id -> ' + i_object.toString();
+    }
+
+    return map;
 }
 
 
